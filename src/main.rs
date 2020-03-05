b575e4cf9cc943af33e1e1e0692a656fa1e2bce1
Max Hargreaves <max_hargreaves@outlook.com>
2020-03-05 19:45:45 +0000

    cant get matching to work, some move issue

--- src/main.rs
+++ src/main.rs
@@ -1,183 +1,254 @@
-use std::collections::HashMap;
+use keyed_priority_queue::KeyedPriorityQueue;
+use std::cmp::{Ord, Ordering, PartialOrd, Reverse};
 
 static CORRECT_ORDER: [i32; 16] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
 static ROW: [i32; 16] = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3];
 static COLUMN: [i32; 16] = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3];
 
 #[derive(Debug, Clone)]
-struct Board {
-    order: [i32; 16],
+struct State {
     moves: String,
     f: i32,
-    g: i32,
     h: i32,
 }
 
-impl std::fmt::Display for Board {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(
-            f,
-            "{:?}\n{:?}\n{:?}\n{:?}",
-            &self.order[0..4],
-            &self.order[4..8],
-            &self.order[8..12],
-            &self.order[12..16]
-        )
+// impl std::fmt::Display for State {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         write!(
+//             f,
+//             "{:?}\n{:?}\n{:?}\n{:?}",
+//             &self.order[0..4],
+//             &self.order[4..8],
+//             &self.order[8..12],
+//             &self.order[12..16]
+//         )
+//     }
+// }
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.h.cmp(&other.h))
     }
 }
 
-impl Board {
-    fn new() -> Board {
-        let new_order: [i32; 16] = [15, 14, 1, 6, 9, 11, 4, 12, 0, 10, 7, 3, 13, 8, 5, 2];
+impl Eq for State {}
 
-        let current_dist = calc_distance(&new_order, &CORRECT_ORDER);
+impl PartialEq for State {
+    fn eq(&self, other: &Self) -> bool {
+        self.h.cmp(&other.h) == Ordering::Equal
+    }
+}
 
-        Board {
-            order: new_order,
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.h.partial_cmp(&other.h).unwrap()
+    }
+}
+
+impl State {
+    fn init(order: &[i32; 16]) -> State {
+        State {
             moves: String::from(""),
             f: 0,
-            g: current_dist,
-            h: 0,
+            h: State::calc_g(&order),
         }
     }
 
-    fn neighbours(&self) -> Vec<Board> {
-        let mut new_boards: Vec<Board> = Vec::new();
-        let current_index = find_index(&self.order, &0);
-        if COLUMN[current_index] > 0 {
-            new_boards.push(self.make_move(left))
-        }
-        if COLUMN[current_index] < 3 {
-            new_boards.push(self.make_move(right))
-        }
-        if ROW[current_index] > 0 {
-            new_boards.push(self.make_move(up))
-        }
-        if ROW[current_index] < 3 {
-            new_boards.push(self.make_move(down))
-        }
-        new_boards
+    fn find_index(order: &[i32; 16], tile: &i32) -> usize {
+        order.iter().position(|&x| x == *tile).unwrap()
     }
 
-    fn make_move(&self, dir: fn(usize, [i32; 16]) -> ([i32; 16], String)) -> Board {
-        let current_index = find_index(&self.order, &0);
-        let (new_order, from) = dir(current_index, self.order);
-        let new_f = self.f + 1;
-        let new_g = calc_distance(&new_order, &CORRECT_ORDER);
-        Board {
-            order: new_order,
-            moves: format!("{}{}", &self.moves, &from),
-            f: new_f,
-            g: new_g,
-            h: new_f + new_g,
-        }
-    }
-}
-
-fn left(index: usize, mut order: [i32; 16]) -> ([i32; 16], String) {
-    order.swap(index, index - 1);
-    return(order, String::from("l"))
-}
-
-fn right(index: usize, mut order: [i32; 16]) -> ([i32; 16], String)  {
-    order.swap(index, index + 1);
-    return(order, String::from("r"))
-}
-
-fn up(index: usize, mut order: [i32; 16]) -> ([i32; 16], String)  {
-    order.swap(index, index - 4);
-    return(order, String::from("u"))
-}
-
-fn down(index: usize, mut order: [i32; 16]) -> ([i32; 16], String)  {
-    order.swap(index, index + 4);
-    return(order, String::from("d"))
-}
-
-
-fn find_index(order: &[i32; 16], tile: &i32) -> usize {
-    order.iter().position(|&x| x == *tile).unwrap()
-}
-
-fn calc_distance(current: &[i32; 16], correct: &[i32; 16]) -> i32 {
-    let mut dist = 0;
-    for tile in correct.iter() {
-        if *tile != 0i32 {
-            let current_index = find_index(current, &tile);
-            let correct_index = find_index(correct, &tile);
-            dist += (COLUMN[current_index] - COLUMN[correct_index]).abs()
+    fn calc_g(current: &[i32; 16]) -> i32 {
+        let mut g = 0;
+        for tile in current.iter() {
+            let current_index = State::find_index(current, &tile);
+            let correct_index = State::find_index(&CORRECT_ORDER, &tile);
+            g += (COLUMN[current_index] - COLUMN[correct_index]).abs()
                 + (ROW[current_index] - ROW[correct_index]).abs();
         }
-        
+        g
     }
-    dist
-}
 
-fn make_key(board: &Board) -> String {
-    board
-        .order
-        .iter()
-        .fold(String::from(""), |key, x| format!("{}{}", key, x))
+    fn make_move(
+        &self,
+        order: &[i32; 16],
+        dir: fn(usize, [i32; 16]) -> ([i32; 16], String),
+    ) -> ([i32; 16], State) {
+        let mut old_order = *order;
+        let new_f = self.f + 1;
+        let new_g = State::calc_g(order);
+        let (new_order, from) = dir(State::find_index(&old_order, &0), *order);
+        let new_state = State {
+            moves: format!("{}{}", self.moves, &from),
+            f: new_f,
+            h: new_f + new_g,
+        };
+        return (new_order, new_state);
+    }
+
+    fn left(index: usize, mut order: [i32; 16]) -> ([i32; 16], String) {
+        order.swap(index, index - 1);
+        return (order, String::from("l"));
+    }
+
+    fn right(index: usize, mut order: [i32; 16]) -> ([i32; 16], String) {
+        order.swap(index, index + 1);
+        return (order, String::from("r"));
+    }
+
+    fn up(index: usize, mut order: [i32; 16]) -> ([i32; 16], String) {
+        order.swap(index, index - 4);
+        return (order, String::from("u"));
+    }
+
+    fn down(index: usize, mut order: [i32; 16]) -> ([i32; 16], String) {
+        order.swap(index, index + 4);
+        return (order, String::from("d"));
+    }
+
+    fn children(&self, order: &[i32; 16]) -> Vec<([i32; 16], State)> {
+        let index = State::find_index(order, &0);
+        let mut new_states: Vec<([i32; 16], State)> = Vec::new();
+        if COLUMN[index] > 0 {
+            new_states.push(self.make_move(order, State::left))
+        }
+        if COLUMN[index] < 3 {
+            new_states.push(self.make_move(order, State::right))
+        }
+        if ROW[index] > 0 {
+            new_states.push(self.make_move(order, State::up))
+        }
+        if ROW[index] < 3 {
+            new_states.push(self.make_move(order, State::down))
+        }
+        new_states
+    }
 }
 
 fn main() {
-    let mut open_boards = HashMap::new();
-    open_boards.insert(
-        make_key(&Board::new()),
-        Board::new(),
-    );
-    let mut closed_boards = HashMap::new();
-    closed_boards.insert(
-        make_key(&Board::new()),
-        Board::new(),
-    );
+    let mut open_states = KeyedPriorityQueue::<[i32; 16], Reverse<State>>::new();
+    let start_order = [15, 14, 1, 6, 9, 11, 4, 12, 0, 10, 7, 3, 13, 8, 5, 2];
+    open_states.push(start_order, Reverse(State::init(&start_order)));
+    let mut closed_states = KeyedPriorityQueue::<[i32; 16], Reverse<State>>::new();
 
-    'outer: while open_boards.len() > 0 {
-        let parent_board = open_boards.values().min_by_key(|board| board.h).unwrap();
-        let parent_key = make_key(parent_board);
-        let parent_board = open_boards.remove(&parent_key).unwrap();
-        let neighbour_boards = parent_board.neighbours();
-
-        println!("Parent f-score: {}", parent_board.f);
-        println!("Parent g-score: {}", parent_board.g);
-        println!("Parent h-score: {}", parent_board.h);
-
-        for neighbour_board in neighbour_boards {
-            if neighbour_board.order.eq(&CORRECT_ORDER) {
-                println!(
-                    "The new final configuartion of the board is:\n{}",
-                    neighbour_board
-                );
-                println!("The moves used were {}.", neighbour_board.moves);
-                break 'outer;
-            }
-            let neighbour_key = make_key(&neighbour_board);
-            if open_boards.contains_key(&neighbour_key) {
-                let old_board = open_boards.get(&neighbour_key).unwrap();
-                if old_board.h <= neighbour_board.h {
-                    continue;
-                } else {
-                    open_boards.insert(neighbour_key, neighbour_board);
+    'outer: while let Some((parent_order, Reverse(parent_state))) = open_states.pop() {
+        for (child_order, child_state) in parent_state.children(&parent_order) {
+            match open_states.get_priority(&child_order) {
+                None => {
+                    open_states.push(child_order, Reverse(child_state));
                 }
-            } else if closed_boards.contains_key(&neighbour_key) {
-                let old_board = closed_boards.get(&neighbour_key).unwrap();
-                if old_board.h <= neighbour_board.h {
-                    continue;
-                } else {
-                    closed_boards.insert(neighbour_key, neighbour_board);
+                Some(&Reverse(old_state)) if old_state.h > child_state.h => {
+                    open_states.set_priority(&child_order, Reverse(child_state));
                 }
-            } else {
-                open_boards.insert(neighbour_key, neighbour_board);
-            }
+                _ => {}
+            };
+            match closed_states.get_priority(&child_order) {
+                Some(&Reverse(old_state)) if old_state.h > child_state.h => {
+                    open_states.push(child_order, Reverse(child_state));
+                }
+                _ => {}
+            };
         }
-        closed_boards.insert(parent_key, parent_board);
-        println!(
-            "There are {} entries in the open hashmap.",
-            open_boards.len()
-        );
-        println!(
-            "There are {} entries in the closed hashmap.",
-            closed_boards.len()
-        )
+        closed_states.push(parent_order, Reverse(parent_state));
+
+        // for neighbour_State in neighbour_States {
+        //     if neighbour_State.order.eq(&CORRECT_ORDER) {
+        //         println!(
+        //             "The new final configuartion of the State is:\n{}",
+        //             neighbour_State
+        //         );
+        //         println!("The moves used were {}.", neighbour_State.moves);
+        //         break 'outer;
+        //     }
+        //     let neighbour_key = make_key(&neighbour_State);
+        //     if open_States.contains_key(&neighbour_key) {
+        //         let old_State = open_States.get(&neighbour_key).unwrap();
+        //         if old_State.h <= neighbour_State.h {
+        //             continue;
+        //         } else {
+        //             open_States.insert(neighbour_key, neighbour_State);
+        //         }
+        //     } else if closed_States.contains_key(&neighbour_key) {
+        //         let old_State = closed_States.get(&neighbour_key).unwrap();
+        //         if old_State.h <= neighbour_State.h {
+        //             continue;
+        //         } else {
+        //             closed_States.insert(neighbour_key, neighbour_State);
+        //         }
+        //     } else {
+        //         open_States.insert(neighbour_key, neighbour_State);
+        //     }
+        // }
+        // closed_States.insert(parent_key, parent_State);
+        // println!(
+        //     "There are {} entries in the open hashmap.",
+        //     open_States.len()
+        // );
+        // println!(
+        //     "There are {} entries in the closed hashmap.",
+        //     closed_States.len()
+        // )
     }
 }
+
+// fn main() {
+// let mut open_States = HashMap::new();
+// open_States.insert(
+//     make_key(&State::new()),
+//     State::new(),
+// );
+// let mut closed_States = HashMap::new();
+// closed_States.insert(
+//     make_key(&State::new()),
+//     State::new(),
+// );
+
+// 'outer: while open_States.len() > 0 {
+//     let parent_State = open_States.values().min_by_key(|State| State.h).unwrap();
+//     let parent_key = make_key(parent_State);
+//     let parent_State = open_States.remove(&parent_key).unwrap();
+//     let neighbour_States = parent_State.children();
+
+//     println!("Parent f-score: {}", parent_State.f);
+//     println!("Parent g-score: {}", parent_State.g);
+//     println!("Parent h-score: {}", parent_State.h);
+
+//     for neighbour_State in neighbour_States {
+//         if neighbour_State.order.eq(&CORRECT_ORDER) {
+//             println!(
+//                 "The new final configuartion of the State is:\n{}",
+//                 neighbour_State
+//             );
+//             println!("The moves used were {}.", neighbour_State.moves);
+//             break 'outer;
+//         }
+//         let neighbour_key = make_key(&neighbour_State);
+//         if open_States.contains_key(&neighbour_key) {
+//             let old_State = open_States.get(&neighbour_key).unwrap();
+//             if old_State.h <= neighbour_State.h {
+//                 continue;
+//             } else {
+//                 open_States.insert(neighbour_key, neighbour_State);
+//             }
+//         } else if closed_States.contains_key(&neighbour_key) {
+//             let old_State = closed_States.get(&neighbour_key).unwrap();
+//             if old_State.h <= neighbour_State.h {
+//                 continue;
+//             } else {
+//                 closed_States.insert(neighbour_key, neighbour_State);
+//             }
+//         } else {
+//             open_States.insert(neighbour_key, neighbour_State);
+//         }
+//     }
+//     closed_States.insert(parent_key, parent_State);
+//     println!(
+//         "There are {} entries in the open hashmap.",
+//         open_States.len()
+//     );
+//     println!(
+//         "There are {} entries in the closed hashmap.",
+//         closed_States.len()
+//     )
+// }
+// }
